4a3e888c0ab99905b7acd1fc502f488cb7d33291
junjun11023 <jjjunpersonal@gmail.com>
2020-08-21 15:44:24 +0800

    Add Function Call

--- run.py
+++ run.py
@@ -6,14 +6,18 @@ def main():
     genomic = Genomic(path)
 
     ret = genomic.get_gene(6, 100000, 199999)
-    order_0 = genomic.get_order(None, order=0)
-    order_1 = genomic.get_order(None, order=1)
-    order_2 = genomic.get_order(None, order=2)
+    order_0, _ = genomic.get_order(order=0)
+    order_1, _ = genomic.get_order(order=1)
+    order_2, _ = genomic.get_order(order=2)
     print({
         "order 0": order_0,
         "order 1": order_1,
         "order 2": order_2
     })
+
+    prob, path = genomic.get_hmm_prob()
+
+    print("prob: ", prob)
 
 if __name__ == '__main__':
     main()
