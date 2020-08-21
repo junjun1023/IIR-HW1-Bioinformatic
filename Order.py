fc42859588b464ec58d2e02aa225671886a4bd04
junjun11023 <jjjunpersonal@gmail.com>
2020-08-21 15:46:17 +0800

    Add hmm function

--- Order.py
+++ Order.py
@@ -3,6 +3,51 @@ import math
 
 class Genomic(object):
 
+    _start = {
+                's1': 0.3,
+                's2': 0.3,
+                's3': 0.4
+            }
+    _transmission = {
+                's1': {
+                    's1': 0.5,
+                    's2': 0.25,
+                    's3': 0.25
+                },
+                's2': {
+                    's1': 0.25,
+                    's2': 0.5,
+                    's3': 0.25
+                },
+                's3': {
+                    's1': 0.25,
+                    's2': 0.25,
+                    's3': 0.5
+                }
+            }
+    _emission = {
+                's1': {
+                    'a': 0.4,
+                    't': 0.2,
+                    'c': 0.2,
+                    'g': 0.2
+                },
+                's2': {
+                    'a': 0.2,
+                    't': 0.4,
+                    'c': 0.2,
+                    'g': 0.2
+                },
+                's3': {
+                    'a': 0.2,
+                    't': 0.2,
+                    'c': 0.4,
+                    'g': 0.2
+                }
+            }
+    _states = ('s1', 's2', 's3')
+
+
     def __init__(self, path):
         super(Genomic, self).__init__()
         self._path = path
@@ -58,7 +103,7 @@ class Genomic(object):
 
 
 
-    def get_order(self, source, order=0, base='atcg'):
+    def get_order(self, order=0, base='atcg', source=None):
 
         key_value = {}
 
@@ -71,17 +116,12 @@ class Genomic(object):
         if source == None:
             if self._source == None:
                 print('Error:- Missing source, execute function -- get_gene first')
+                return
             else:
                 source = self._source
 
         source = source.lower()
         total = len(source)
-
-        # Add additional keys
-        # For leading only
-        # for i in range(order):
-        #     k = source[0:i+1]
-        #     key_value[k] = 0
 
         for i in range(total):
             for j in range(order+1):
@@ -121,7 +161,85 @@ class Genomic(object):
         # print(prob)
             
 
-        return prob
+        return prob, key_value
         
 
+    
+    def get_hmm_prob(self, states=None, start=None, transmission=None, emission=None, source=None):
+        # 在這個狀態往下一個狀態看
+        if source == None:
+            if self._source == None:
+                print('Error:- Missing source, execute function -- get_gene first')
+                return
+            else:
+                source = self._source
+        source = source.lower()
+
+        if states==None:
+            states = self._states
+
+        if start==None:
+            start = self._start
+
+        if transmission==None:
+            transmission = self._transmission
+        
+        if emission==None:
+            emission = self._emission
+
+        path = {}
+        V = [{}]
+
+
+        # Initialize base cases (t == 0)
+        for y in states:
+            V[0][y] = math.log2(start[y]) + math.log2(emission[y][source[0]])
+            path[y] = [y]
+
+
+        # Run Viterbi for t > 0
+        for t in range(1,len(source)):
+            V.append({})
+            newpath = {}
+
+            for y in states:
+
+                (prob, state) = max( [(V[t-1][y0] + math.log2(transmission[y0][y]) + math.log2(emission[y][source[t]]), y0)  for y0 in states])
+                # print(prob, state)
+                
+                V[t][y] = prob
+
+                # print(path)
+                # print(state)
+                # print(path[state])
+                newpath[y] = path[state] + [y]
+
+                # Don't need to remember the old paths
+            path = newpath
+
+        (prob, state) = max([(V[len(source) - 1][y], y) for y in states])
+
+        fp = open("state_path.txt", "w")
+        fp.write(str(path[state]))
+        fp.close()
+
+        return (prob, path[state])
+
+
+
+
+
+
+
+
+
+    # def _source_valid(self, source):
+    #     if source == None:
+    #         if self._source == None:
+    #             self.
+    #             return 
+    #         else:
+    #             source = self._source
+    #             return source
+    #     return 
     
