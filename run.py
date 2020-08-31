3b585386b12c37ed949b9a733df6269d911d71f0
junjun11023 <jjjunpersonal@gmail.com>
2020-08-31 09:47:00 +0800

    Add running time info.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import psutil
+import time
 from Order import Genomic
 
 current_process = psutil.Process(os.getpid())
@@ -9,21 +10,37 @@ def main():
     path = "GRCh38_latest_genomic.fna"
     genomic = Genomic(path)
 
+    start_time = time.time()
+    start_mem = current_process.memory_info().rss # in byte
     ret = genomic.get_gene(6, 100000, 199999)
     end_mem = current_process.memory_info().rss
-    f.write("Done Extract: " + str(end_mem) + "\n")
+    end_time = time.time()
+    f.write("Done Extract: " + str(end_mem-start_mem) + "bytes \n")
+    f.write("Done Extract: " + str(end_time-start_time) + "sec \n")
 
+    start_time = time.time()
+    start_mem = current_process.memory_info().rss # in byte
     order_0, _ = genomic.get_order(order=0)
     end_mem = current_process.memory_info().rss
-    f.write("Done Order 0: " + str(end_mem) + "\n")
+    end_time = time.time()
+    f.write("Done Order 0: " + str(end_mem-start_mem) + "bytes \n")
+    f.write("Done Order 0: " + str(end_time-start_time) + "sec \n")
 
+    start_time = time.time()
+    start_mem = current_process.memory_info().rss # in byte
     order_1, _ = genomic.get_order(order=1)
     end_mem = current_process.memory_info().rss
-    f.write("Done Order 1: " + str(end_mem) + "\n")
+    end_time = time.time()
+    f.write("Done Order 1: " + str(end_mem-start_mem) + "bytes \n")
+    f.write("Done Order 1: " + str(end_time-start_time) + "sec \n")
 
+    start_time = time.time()
+    start_mem = current_process.memory_info().rss # in byte
     order_2, _ = genomic.get_order(order=2)
     end_mem = current_process.memory_info().rss
-    f.write("Done Order 2: " + str(end_mem) + "\n")
+    end_time = time.time()
+    f.write("Done Order 2: " + str(end_mem-start_mem) + "bytes \n")
+    f.write("Done Order 2: " + str(end_time-start_time) + "sec \n")
 
     print({
         "order 0": order_0,
@@ -31,16 +48,17 @@ def main():
         "order 2": order_2
     })
 
+    start_time = time.time()
+    start_mem = current_process.memory_info().rss # in byte
     prob, path = genomic.get_hmm_prob()
     end_mem = current_process.memory_info().rss
-    f.write("Done HMM: " + str(end_mem) + "\n")
+    end_time = time.time()
+    f.write("Done HMM: " + str(end_mem-start_mem) + "bytes \n")
+    f.write("Done HMM: " + str(end_time-start_time) + "sec \n")
 
     print("prob: ", prob)
 
 if __name__ == '__main__':
-
-    start_mem = current_process.memory_info().rss # in byte
-    f.write("main:- start " + str(start_mem) + "\n")
 
     main()
 
